7d0f48d5bdf208a0c7febf4f475cf5600ba6a2a7
Angogi <andresmahanaim@gmail.com>
2019-08-09 11:38:22 +0200

    crear_carpeta_Y_archivoconnet

--- wheel.sql
+++ wheel.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-08-2019 a las 09:50:24
--- Versión del servidor: 10.3.16-MariaDB
--- Versión de PHP: 7.1.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,32 +5,15 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `wheel`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `user`
---
-
-CREATE TABLE `user` (
+CREATE TABLE `doom_users` (
   `user_id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `dead` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `user`
---
 
-INSERT INTO `user` (`user_id`, `name`, `dead`) VALUES
+INSERT INTO `wheel` (`user_id`, `name`, `dead`) VALUES
 (4, 'Alexandro', 0),
 (5, 'Arnau', 0),
 (6, 'Camilo', 0),
@@ -63,27 +38,11 @@ INSERT INTO `user` (`user_id`, `name`, `dead`) VALUES
 (25, 'Andres', 0),
 (26, 'Thierry', 0);
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
+ALTER TABLE `doom_users`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
+ALTER TABLE `doom_users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
